7517fd6956212a0ecdac961bcf1567c38cb9351e
Marcos Garcia <marcos.garcia.rodriguez.al@iespoligonosur.org>
2025-11-06 14:37:33 +0100

    Cambios finales reparacion tarea 2

--- camping/views.py
+++ camping/views.py
@@ -21,7 +21,7 @@ def ver_campings(request):
 
 #   Ordenar las reservas por fecha de inicio
 def ver_reservas_por_fecha(request):
-    reservas = Reserva.objects.select_related('parcela__camping').select_related('cliente__datos_cliente').order_by('fecha_inicio').all()
+    reservas = Reserva.objects.select_related('cliente__datos_cliente','parcela__camping').order_by('fecha_inicio').all()
     
     """
     reservas = (Reserva.objects.raw("SELECT cr.id AS id, cp.nombre, cp.apellido, cr.fecha_inicio, cr.fecha_fin  FROM camping_reserva cr "
